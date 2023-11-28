b50a2c18e5e4cb886d8b27945248c81fec33a4c8
Niels Theijs <niels.theijs@choicehotels.com>
2023-11-28 14:03:10 -0700

    MR - Initial commit

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Niels"
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello, {this.state.name}
+          </p>
+          <button>
+          increment
+          </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
