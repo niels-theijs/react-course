436fd9ef7a58f037d4ef2da85d6ec71617c4aa77
Niels Theijs <theijsniels@gmail.com>
2023-12-04 23:20:17 -0700

    video 44 - optimizations

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -12,36 +12,47 @@ class App extends Component {
     }
   }
 
-componentDidMount() {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => 
-      this.setState(
-        () => {
-          return {monsters: users};
-        },
-        () => {
-          console.log(this.state)
-        }
-      )
-    );
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    
+    this.setState(() => {
+      return { searchField };
+    });
   }
+  
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => 
+        this.setState(
+          () => {
+            return {monsters: users};
+          },
+          () => {
+            console.log(this.state)
+          }
+        )
+      );
+    }
 
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes( this.state.searchField )
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes( searchField )
     })
 
     return (
       <div className="App">
-        <input className="search-box" type="search" placeholder="search monsters" 
-        onChange={(event) => {
-          const searchField = event.target.value.toLowerCase()
-          
-          this.setState(() => {
-            return { searchField }
-          })
-        }}/>
+        <input 
+          className="search-box" 
+          type="search" 
+          placeholder="search monsters" 
+          onChange={ onSearchChange }
+        />
+
         {
           filteredMonsters.map((monster) => {
             return <div key={monster.id}>
