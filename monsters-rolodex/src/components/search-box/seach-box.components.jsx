ebee3ee161c7f6e7ee07b126a2550904f33ddbdc
Niels Theijs <nielstheijs@icloud.com>
2023-12-11 12:24:38 -0700

    functional components - vid 62

--- monsters-rolodex/src/components/search-box/seach-box.components.jsx
+++ monsters-rolodex/src/components/search-box/seach-box.components.jsx
@@ -1,18 +1,13 @@
-import { Component } from "react";
-
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-    render() {
-        return (
-            <input 
-            className={`search-box ${ this.props.className }`}
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+        <input 
+            className={`search-box ${ className }`}
             type="search" 
-            placeholder={ this.props.placeholder }
-            onChange={ this.props.onChangeHandler }
-            />
-        )
-    }
-}
+            placeholder={ placeholder }
+            onChange={ onChangeHandler }
+        />
+    )
+
 
 export default SearchBox;
