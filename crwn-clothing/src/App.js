cdff79604f2742444b4c928db8b2b6ea7a4de180
Niels Theijs <niels.theijs@choicehotels.com>
2023-12-12 13:53:14 -0700

    76 - Capstone Scaffolding

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,47 @@
+const App = () => {
+  return (
+    <div className="catagories-container">
+      <div classname="category-container">
+        { /* <img /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div classname="category-container">
+        { /* <img /> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div classname="category-container">
+        { /* <img /> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div classname="category-container">
+        { /* <img /> */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div classname="category-container">
+        { /* <img /> */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default App;
